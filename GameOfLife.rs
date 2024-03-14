138bddc3f0dee7e6170d70e4f8852682ed0389c9
olavo.m <olavo.m@outlook.com.br>
2024-03-14 20:14:06 -0300

    input

--- GameOfLife.rs
+++ GameOfLife.rs
@@ -7,7 +7,7 @@ fn clear_screen() {
 fn imprimir_matriz(matriz: &mut [[bool; TAM]; TAM]) {
     println!("  _0|_1|_2|_3|_4|_5|_6|_7|_8|_9|10|11|12|13|14|");
     for i in 0..TAM {
-        print!("{} ", (i + 65) as char); // Letras das linhas
+        print!("{} ", (i as u8 + 65) as char); // Letras das linhas
 
         for j in 0..TAM {
             if matriz[i][j] {
@@ -26,13 +26,16 @@ fn main() {
     let mut matriz_futura: [[bool; TAM]; TAM] = [[false; TAM]; TAM];
 
     loop {
-        cls();
+        clear_screen();
         println!("Jogo da vida de Conway\n");
-        imprimirMatriz(matriz_atual);
+        imprimir_matriz(matriz_atual);
         println!("\n\n\nEscolha uma opcao:");
         println!("0 - Sair");
         println!("1 - Preencher manualmente a matriz");
         println!("2 - Usar um padrao pronto");
+        let mut opcao = String::new();
+        io::stdin().read_line(&mut opcao).expect("Falha ao ler a opção");
+        let opcao: int = match opcao.trim().parse() 
 
         if opcao == 0 {
             break;
