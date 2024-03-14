0b564c5be8a3d74198869a699b1a5755ca3217c1
olavo.m <olavo.m@outlook.com.br>
2024-03-14 19:56:06 -0300

    Menu inicial

--- GameOfLife.rs
+++ GameOfLife.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Test");
+fn cls() {
+    std::process::Command::new("sh").arg("-c").arg("clear").status().unwrap();
 }
+
+fn main() {
+    let mut matriz_atual: [[bool; 5]; 5] = [[false; 5]; 5];
+    let mut matriz_futura: [[bool; 5]; 5] = [[false; 5]; 5];
+
+    loop {
+        cls();
+        println!("Jogo da vida de Conway\n");
+        imprimirMatriz(matriz_atual);
+        println!("\n\n\nEscolha uma opcao:");
+        println!("0 - Sair");
+        println!("1 - Preencher manualmente a matriz");
+        println!("2 - Usar um padrao pronto");
+
+        if (opcao == 0) {
+            break;
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Conway-GameOfLife-Rust
 My old C Game of Life translated to the Rust Language
+
+
+<br>
+<br>
+### TODO:
+
+- [ ] Usar uma lib para limpar a tela (cls), que seja multiplataforma
